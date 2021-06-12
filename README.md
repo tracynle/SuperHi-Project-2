dd3a7c0454669bf8939cd2ecb20d7716c5568427
Tracy Le <tnle9@asu.edu>
2021-06-11 17:11:13 -0700

    Assets

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SuperHi-Project-2
- Shopify Theme Development Tutorial Week 2 - Wellby. This project will demonstrate my understanding on how to create a shop containing a collection of products on Shopify. I will learning how to import products, create modular components, build dynamic content with pages and link lists, use advanced SCSS features, and Shopify's AJAX API.
+ Shopify Theme Development Tutorial Week 2 - Wellby. This project will demonstrate my understanding on how to create a shop containing a collection of products on Shopify. I will be learning how to import products, create modular components, build dynamic content with pages and link lists, use advanced SCSS features and Shopify's AJAX API.
--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,78 @@
-// Put your application javascript here
+$(document).ready(function() {
+
+    // Increase or decrease the quantity
+    $(document).on('click', '.js-quantity-button', function(event) {
+        let 
+            $button = $(this),
+            $form = $button.closest('form'),
+            $quantity = $form.find('.js-quantity-field'),
+            quantityValue = parseInt($quantity.val()),
+            max = $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;  
+
+        if ($button.hasClass('plus') && (max === null || quantityValue + 1 <= max)) {
+            // do something for plus click
+            $quantity.val(quantityValue + 1).change();
+            console.log("clicked plus");
+        }
+        else if ($button.hasClass('minus')) {
+            // do something for minus click
+            $quantity.val(quantityValue - 1).change();
+            console.log("clicked minus");
+
+        }
+    });
+    
+    // Disable or enable quantity buttons 
+    $(document).on('change', '.js-quantity-field', function(event) {
+        let 
+            $field = $(this),
+            $form = $field.closest('form'),
+            $quantityText = $form.find('.js-quantity-text'),
+            shouldDisableMinus = parseInt(this.value) === 1,
+            shouldDisablePlus = parseInt(this.value) === parseInt($field.attr('max')),
+            $minusButton = $form.find('.js-quantity-button.minus');
+            $plusButton = $form.find('.js-quantity-button.plus');
+
+        // set the text of the quantity text element to the value of the field
+        $quantityText.text(this.value);
+
+        if(shouldDisableMinus) {
+            $minusButton.prop('disabled', true);
+        }
+        // if button is currently disabled
+        else if ($minusButton.prop('disabled') === true) {
+            // enable the button 
+            $minusButton.prop('disabled', false);
+        }
+
+        if(shouldDisablePlus) {
+            $plusButton.prop('disabled', true);
+        }
+        // if button is currently disabled
+        else if ($plusButton.prop('disabled') === true) {
+            // enable the button 
+            $plusButton.prop('disabled', false);
+        }
+    });
+
+    // update the quantity of the selected radio button
+    $(document).on('change', '.js-variant-radio', function(event) { 
+        let  
+            $radio = $(this),
+            $form = $radio.closest('form'),
+            max = $radio.attr('data-inventory-quantity'),
+            $quantity = $form.find('.js-quantity-field'),
+            $addToCartButton = $form.find('#add-to-cart-button');
+        
+        if ($addToCartButton.prop('disabled') === true) {
+            $addToCartButton.prop('disabled', false);
+        }
+
+
+        $quantity.attr('max', max);
+
+       if (parseInt($quantity.val()) > max) {
+           $quantity.val(max).change();
+       }
+    });
+});
