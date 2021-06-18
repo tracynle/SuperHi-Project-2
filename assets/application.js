b32520c97ab5487b7e7010f3216ad5a34efdc5d8
Tracy Le <tnle9@asu.edu>
2021-06-18 14:12:26 -0700

    Mini cart feature and font styles added
    Fully functioning cart.

--- assets/application.js
+++ assets/application.js
@@ -1,104 +1,149 @@
+// Put your applicaiton javascript here
 $(document).ready(function() {
-   
-    let 
-        onQuantityButtonClick = function(event) {
-            let 
-                $button = $(this),
-                $form = $button.closest('form'),
-                $quantity = $form.find('.js-quantity-field'),
-                quantityValue = parseInt($quantity.val()),
-                max = $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;  
 
-            if ($button.hasClass('plus') && (max === null || quantityValue + 1 <= max)) {
-                // do something for plus click
-                $quantity.val(quantityValue + 1).change();
-                console.log("clicked plus");
-            }
-            else if ($button.hasClass('minus')) {
-                // do something for minus click
-                $quantity.val(quantityValue - 1).change();
-                console.log("clicked minus");
-
-            }
-        },
-        onQuantityFieldChange = function(event) {
-            let 
-                $field = $(this),
-                $form = $field.closest('form'),
-                $quantityText = $form.find('.js-quantity-text'),
-                shouldDisableMinus = parseInt(this.value) === 1,
-                shouldDisablePlus = parseInt(this.value) === parseInt($field.attr('max')),
-                $minusButton = $form.find('.js-quantity-button.minus');
-                $plusButton = $form.find('.js-quantity-button.plus');
-    
-            // set the text of the quantity text element to the value of the field
-            $quantityText.text(this.value);
-    
-            if(shouldDisableMinus) {
-                $minusButton.prop('disabled', true);
-            }
-            // if button is currently disabled
-            else if ($minusButton.prop('disabled') === true) {
-                // enable the button 
-                $minusButton.prop('disabled', false);
-            }
-    
-            if(shouldDisablePlus) {
-                $plusButton.prop('disabled', true);
-            }
-            // if button is currently disabled
-            else if ($plusButton.prop('disabled') === true) {
-                // enable the button 
-                $plusButton.prop('disabled', false);
-            }
-        },
-        onVariantRadioChange = function(event) { 
-            let  
-                $radio = $(this),
-                $form = $radio.closest('form'),
-                max = $radio.attr('data-inventory-quantity'),
-                $quantity = $form.find('.js-quantity-field'),
-                $addToCartButton = $form.find('#add-to-cart-button');
+    let
+      onQuantityButtonClick = function(event) {
+        // alert('button clicked');
+        let
+          $button = $(this),
+          $form = $button.closest('form'),
+          $quantity = $form.find('.js-quantity-field'),
+          quantityValue = parseInt($quantity.val()),
+          max = $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;
+  
+        if ($button.hasClass('plus') && (max === null || quantityValue+1 <= max)) {
+          // do something for plus click
+          $quantity.val(quantityValue + 1).change();
+        }
+        else if ($button.hasClass('minus')) {
+          // do something for minus click
+          $quantity.val(quantityValue - 1).change();
+        }
+      },
+      onQuantityFieldChange = function(event) {
+        let
+          $field = $(this),
+          $form = $field.closest('form'),
+          $quantityText = $form.find('.js-quantity-text'),
+          shouldDisableMinus = parseInt(this.value) === 1,
+          shouldDisablePlus = parseInt(this.value) === parseInt($field.attr('max')),
+          $minusButton = $form.find('.js-quantity-button.minus'),
+          $plusButton = $form.find('.js-quantity-button.plus');
+  
+        $quantityText.text(this.value);
+  
+        if (shouldDisableMinus) {
+          $minusButton.prop('disabled', true);
+        }
+        else if ($minusButton.prop('disabled') === true) {
+          $minusButton.prop('disabled', false);
+        }
+  
+        if (shouldDisablePlus) {
+          $plusButton.prop('disabled', true);
+        }
+        else if ($plusButton.prop('disabled') === true) {
+          $plusButton.prop('disabled', false);
+        }
+      },
+      onVariantRadioChange = function(event) {
+        let
+          $radio = $(this),
+          $form = $radio.closest('form'),
+          max = $radio.attr('data-inventory-quantity'),
+          $quantity = $form.find('.js-quantity-field'),
+          $addToCartButton = $form.find('#add-to-cart-button');
+        
+        if ($addToCartButton.prop('disabled') === true) {
+          $addToCartButton.prop('disabled', false);
+        }
+  
+        $quantity.attr('max', max);
+  
+        if (parseInt($quantity.val()) > max) {
+          $quantity.val(max).change();
+        }
+      },
+      onAddToCart = function(event) {
+        event.preventDefault();
+  
+        $.ajax({
+          type: 'POST',
+          url: '/cart/add.js',
+          data: $(this).serialize(), // creates a text string in standard url encoded notation
+          dataType: 'json',
+          success: onCartUpdated,
+          error: onError
+        });
+      },
+      onLineRemoved = function(event) { // removes line item item. refer to line-item.liquid
+        event.preventDefault();
+        
+        let
+          $removeLink = $(this),
+          removeQuery = $removeLink.attr('href').split('change?')[1];
+        $.post('/cart/change.js', removeQuery, onCartUpdated, 'json'); // post(url, data, success, datatype)
+      },
+      onCartUpdated = function() {
+        $.ajax({
+          type: 'GET',
+          url: '/cart',
+          context: document.body,
+          success: function(context) {
+            let
+              $dataCartContents = $(context).find('.js-cart-page-contents'),
+              dataCartHtml = $dataCartContents.html(), //turns the content inside of this element into a string that we can output anywhere else
+              dataCartItemCount = $dataCartContents.attr('data-cart-item-count'),
+              $miniCartContents = $('.js-mini-cart-contents'),
+              $cartItemCount = $('.js-cart-item-count');
             
-            if ($addToCartButton.prop('disabled') === true) {
-                $addToCartButton.prop('disabled', false);
+            // update item count
+            $cartItemCount.text(dataCartItemCount);
+            $miniCartContents.html(dataCartHtml); // replace the html with the datacart html 
+  
+            if (parseInt(dataCartItemCount) > 0) {
+              openCart();
             }
-    
-    
-            $quantity.attr('max', max);
-    
-           if (parseInt($quantity.val()) > max) {
-               $quantity.val(max).change();
-           }
-        },
-        onAddToCart = function(event) {
-            event.preventDefault(); // gets the event and not execute the behavior. prevents going to the cart after adding item
-            $.ajax({
-                type: 'POST',
-                url: '/cart/add.js',
-                data: $(this).serialize(),  // send data to the url using the form using serialize()
-                dataType: 'json',
-                success:  onCartUpdated,
-                error: onError
-            })
-        },
-        onCartUpdated = function() {
-            alert('cart is updated');
-        },
-        onError = function(XMLHttpRequest, textStatus) {
-            let data = XMLHttpRequest.responseJSON;
-            alert(data.status + ' - ' + data.message + ': ' + data.description);
-        };
-
-
-    // Increase or decrease the quantity
+            else {
+              closeCart();
+            }
+          }
+        });
+      },
+      onError = function(XMLHttpRequest, textStatus) {
+        let data = XMLHttpRequest.responseJSON;
+        alert(data.status + ' - ' + data.message + ': ' + data.description);
+      },
+      openCart = function() {
+        $('html').addClass('mini-cart-open');
+      },
+      closeCart = function() {
+        $('html').removeClass('mini-cart-open');
+      },
+      onCartButtonClick = function(event) {
+        event.preventDefault();
+  
+        let isCartOpen = $('html').hasClass('mini-cart-open');
+  
+        if (!isCartOpen) {
+          openCart();
+        }
+        else {
+          closeCart();
+        }
+      };
+      
+  
     $(document).on('click', '.js-quantity-button', onQuantityButtonClick);
-    
-    // Disable or enable quantity buttons 
+  
     $(document).on('change', '.js-quantity-field', onQuantityFieldChange);
-
-    // update the quantity of the selected radio button
+  
     $(document).on('change', '.js-variant-radio', onVariantRadioChange);
-
+  
     $(document).on('submit', '#add-to-cart-form', onAddToCart);
-});
+  
+    $(document).on('click', '#mini-cart .js-remove-line', onLineRemoved);
+  
+    $(document).on('click', '.js-cart-link, #mini-cart .js-keep-shopping', onCartButtonClick);
+  
+  });
